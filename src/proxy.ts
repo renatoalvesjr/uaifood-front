e23b99b8b26f88ef27f8b65bb48645bf46c391dc
renatoalvesjr <renato.alvesjr00@gmail.com>
2025-11-19 22:50:38 -0300

    middleware to proxy

---
+++ src/proxy.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const protectedRoutes = ["/profile","/dashboard","/admin"];
+
+export function proxy(request: NextRequest) {
+  const accessToken = request.cookies.get("access_token")?.value;
+
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  );
+
+  if (isProtectedRoute && !accessToken) {
+    const loginUrl = new URL("/login", request.url);
+
+    loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
+
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile/:path*","/dashboard/:path*", "/login","/admin/:path*"],
+};
